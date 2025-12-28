39775bd7def03bd3c5a83178af100ab1de5f244d
uma-uc <uma.apple123@gmail.com>
2025-12-27 22:46:00 -0800

    feat: remove eslint files

--- packages/ajv/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("eslint").Linter.Config} */
-module.exports = {
-  root: true,
-  extends: ["@bwalk/eslint-config/index.js"],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: true,
-  },
-};
