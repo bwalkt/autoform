ca1537fd20772654d953dc7da50b519dd0b5b567
uma-uc <uma.apple123@gmail.com>
2026-02-08 11:51:53 -0800

    feat: grid css wip

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -96,4 +96,28 @@ module.exports = {
     },
   },
   plugins: [],
+  safelist: [
+    // Grid columns
+    'grid-cols-1',
+    'grid-cols-2',
+    'grid-cols-3',
+    'grid-cols-4',
+    'grid-cols-5',
+    'grid-cols-6',
+    'grid-cols-7',
+    'grid-cols-8',
+    'grid-cols-9',
+    'grid-cols-10',
+    'grid-cols-11',
+    'grid-cols-12',
+    'grid-cols-none',
+    // Grid rows
+    'grid-rows-1',
+    'grid-rows-2',
+    'grid-rows-3',
+    'grid-rows-4',
+    'grid-rows-5',
+    'grid-rows-6',
+    'grid-rows-none',
+  ],
 }
