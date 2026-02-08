eaee085d749728e78e148945a7abc899ecee188a
uma-uc <uma.apple123@gmail.com>
2026-02-07 19:29:47 -0800

    refactor: align layout exports

--- packages/ui/src/layouts/index.ts
+++ packages/ui/src/layouts/index.ts
@@ -1,10 +1,10 @@
 'use client'
 
 // Layout Components
-export { Box, type BoxOwnProps, type BoxProps } from './Box'
-export { Container, type ContainerOwnProps, type ContainerProps } from './Container'
-export { Flex, type FlexOwnProps, type FlexProps } from './Flex'
-export { Grid, type GridOwnProps, type GridProps } from './Grid'
+export { Box, type BoxProps } from './Box'
+export { Container, type ContainerProps } from './Container'
+export { Flex, type FlexProps } from './Flex'
+export { Grid, type GridProps } from './Grid'
 // Layout Utilities and Types
 export {
   type AlignItems,
@@ -38,7 +38,7 @@ export {
   type Spacing,
   spacingScale,
 } from './layout-utils'
-export { Section, type SectionOwnProps, type SectionProps } from './Section'
+export { Section, type SectionProps } from './Section'
 
 // Import CSS
 import './layouts.css'
